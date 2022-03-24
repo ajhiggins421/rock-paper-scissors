c2c3494de38169f7d331b4d4f4ef736a91711c7f
ajhiggins <ajhigginselite@gmail.com>
2022-03-24 04:03:04 -0400

    added ui and js functionality

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ let choices = ["rock", "paper", "scissors"];
 let compScore = 0;
 let playerScore = 0;
 
+const playerScoreText = document.querySelector("#playerScore");
+const compScoreText = document.querySelector("#compScore");
+const playerImage = document.querySelector("#playerImage");
+const compImage = document.querySelector("#compImage");
+
 function computerPlay(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
@@ -11,8 +16,14 @@ function getInput() {
     return choice.toLowerCase();
 }
 
+function updateImages(compChoice, playerChoice){
+    compImage.src = `assets/${compChoice}.png`;
+    playerImage.src = `assets/${playerChoice}.png`;
+
+}
 function play(computerChoice, playerChoice){
     let winLoseOrTie;
+    updateImages(computerChoice, playerChoice);
     if(choices.includes(playerChoice)){
 
         if(computerChoice == playerChoice){
@@ -22,39 +33,35 @@ function play(computerChoice, playerChoice){
         else if((computerChoice == "rock" && playerChoice == "scissors") || (computerChoice == "paper" && playerChoice =="rock") || 
                 (computerChoice == "scissors" && playerChoice == "paper")){
             winLoseOrTie = "lose";
+            compScoreText.textContent = compScore + 1;
             compScore++;
         }
 
         else {
             winLoseOrTie = "win";
+            playerScoreText.textContent = playerScore +1;
             playerScore++;
         }
-        return "Computer chooses " + computerChoice + " and you choose " + playerChoice +" so you " + winLoseOrTie;
     }
 
-    else return "Invalid choice";
 }
 
-function game(){
-    for(let i = 0; i<5; i++){
-        let playerChoice = getInput();
-        console.log(play(computerPlay(), playerChoice));
+function game(playerChoice){
+    if(compScore == 5 || playerScore ==5) {
+        compScore > playerScore ? alert("Computer wins!") : alert("You win!");
+        playerScore = 0;
+        compScore = 0;
+        playerScoreText.textContent = 0;
+        compScoreText.textContent = 0;
+        playerImage.src = "assets/question-mark.jpg"
+        compImage.src = "assets/question-mark.jpg"
     }
+    else play(computerPlay(), playerChoice);
 
-    console.log("You: " + playerScore + "\nComputer: " + compScore);
-
-    if(compScore > playerScore) {
-        console.log("\nYou lose!");
-    }
-    if(playerScore > compScore) {
-        console.log("\nYou win!");
-    }
-    else {
-        console.log("\nIts a Draw");
-    }
-
-    compScore = 0;
-    playerScore = 0;
 }
 
-game();
+buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => game(button.id));
+});
